d3b3e804d050a268d37141d0ea645ca09363d46a
Daniel Seeberg Hådem <DanielSeebergH@gmail.com>
2024-11-26 13:31:15 +0100

    Remove all ignored files from Git tracking

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5542cf083db7ecc314829bc87e02f9706f00f13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a631210ef4c462ccb0cffd766016af2afa5df61d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
